4166aa06dbcf055f73cdb6ebae20afbaec3b952d
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2020-09-09 16:21:08 +0300

    Reorder parameters in method containsPoint

--- src/main/kotlin/shapes/Rectangle.kt
+++ src/main/kotlin/shapes/Rectangle.kt
@@ -17,7 +17,7 @@ class Rectangle(val width: Double, val height: Double) : Shape {
         return shapeName
     }
 
-    fun containsPoint(point: Point, shape: Shape) {
+    fun containsPoint(shape: Shape, point: Point) {
         // TODO: implement it
     }
 }
