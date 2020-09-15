67c0bfffc84088dcdbb88a2b7bcc90aec3e764a9
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2020-09-15 18:55:59 +0300

    Pull Up Method

--- src/main/kotlin/shapes/Square.kt
+++ src/main/kotlin/shapes/Square.kt
@@ -2,11 +2,6 @@ package shapes
 
 class Square(val len: Double) : Rectangle(len, len) {
 
-    fun getDescription(): String {
-        val text = "The area of the shape is measured by multiplying the width times the height."
-        return text
-    }
-
     override fun getName(): String {
         return "Square"
     }
