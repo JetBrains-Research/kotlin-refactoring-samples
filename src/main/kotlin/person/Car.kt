99f12c40f4a3d73b294f201d2bf0978015b417c9
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2020-09-16 17:51:20 +0300

    Extract Class Car

---
+++ src/main/kotlin/person/Car.kt
@@ -0,0 +1,6 @@
+package person
+
+class Car {
+    var carColor: String = ""
+    var carNumber: String = ""
+}
--- src/main/kotlin/person/Person.kt
+++ src/main/kotlin/person/Person.kt
@@ -1,4 +1,5 @@
+import person.Car
+
 class Person(val name: String, val id: Int) {
-    var carColor: String = ""
-    var carNumber: String = ""
+    var car: Car = TODO()
 }
