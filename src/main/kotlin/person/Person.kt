85f42cbb8cda603dca6544f4c7039ab268256f48
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2020-09-17 20:41:42 +0300

    Inline Method

--- src/main/kotlin/person/Person.kt
+++ src/main/kotlin/person/Person.kt
@@ -4,10 +4,7 @@ class Person(val name: String, val age: Int, val id: Int) {
     var car: Car = TODO()
 
     public fun isAgeValid(): Boolean {
-        return isAgeMoreThanZero() && age < 110
+        return age > 0 && age < 110
     }
 
-    public fun isAgeMoreThanZero(): Boolean {
-        return age > 0;
-    }
 }
