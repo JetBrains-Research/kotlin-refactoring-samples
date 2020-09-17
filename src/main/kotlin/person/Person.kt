6bc496e38beeb70608f99a93485684a47f5a97c2
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2020-09-17 20:41:16 +0300

    Add a couple of methods

--- src/main/kotlin/person/Person.kt
+++ src/main/kotlin/person/Person.kt
@@ -1,5 +1,13 @@
 import person.Car
 
-class Person(val name: String, val id: Int) {
+class Person(val name: String, val age: Int, val id: Int) {
     var car: Car = TODO()
+
+    public fun isAgeValid(): Boolean {
+        return isAgeMoreThanZero() && age < 110
+    }
+
+    public fun isAgeMoreThanZero(): Boolean {
+        return age > 0;
+    }
 }
